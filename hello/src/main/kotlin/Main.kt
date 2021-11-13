64cf6aff24b3af5e13e189ff556f5a54b02fdf6a
Ryo Ishizawa <ryoishizawa298142@gmail.com>
2021-11-13 22:58:32 +0800

    #0 Kotlin practice

--- hello/src/main/kotlin/Main.kt
+++ hello/src/main/kotlin/Main.kt
@@ -15,6 +15,20 @@ fun main(args: Array<String>) {
 
     val pair = "A" to "Z"
     println(pair)
+
+    // array
+    val asc = Array(5) { i -> (i * i).toString() }
+    println("=== array ===")
+    asc.forEach { println(it) }
+    println("=== modify array ===")
+    println(asc[3])
+    asc[3] = "3412"
+    println(asc[3])
+    // compile error
+    // val asc2: Array<Any> = asc
+    // Type projections
+    val asc2: Array<out Any> = asc
+    asc2.forEach { println(it) }
 }
 
 // optional parameter with default value `INFO`
